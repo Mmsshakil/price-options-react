19bd64fc601f4c854cfa7b00aa6c1ac9259b3075
Mmsshakil <softhandshakil@gmail.com>
2023-09-18 18:59:54 +0600

    nav icons added but i don't understand it clearly

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import DaisyNav from './Components/DaisyNav'
 import NavBar from './Components/NavBar/NavBar'
 
+
 function App() {
 
   return (
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { BiAlignRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const NavBar = () => {
 
+    const [open, setOpen] = useState(false);
 
     const routes = [
         { path: '/', name: 'Home', id: 'home' },
@@ -14,6 +18,15 @@ const NavBar = () => {
 
     return (
         <nav>
+            {/* this div will hidden in midium size display 
+            and when i will click on this part - it will set the value of setOpen of opoosite of the previous value  */}
+            <div className="text-2xl md:hidden" onClick={() => setOpen(!open)}>
+                {
+                    // if the open value is true then show the X sign and if the open value false show the manu icon
+                    open === true ? <AiOutlineClose></AiOutlineClose> : <BiAlignRight ></BiAlignRight>
+                }
+                
+            </div>
             <ul className="md:flex">
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
